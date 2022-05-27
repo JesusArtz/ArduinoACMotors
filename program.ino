b888b385c87e0db3d90a4a9a4577c351795de790
Jesus <85129357+JesusArtz@users.noreply.github.com>
2022-05-27 12:54:52 -0500

    Added a servo

--- program.ino
+++ program.ino
@@ -1,3 +1,5 @@
+#include <Servo.h>
+
 #include <AFMotor.h>
 
 AF_DCMotor motor1(1);
@@ -5,6 +7,9 @@ AF_DCMotor motor2(2);
 AF_DCMotor motor3(3);
 AF_DCMotor motor4(4);
 
+Servo sMotor;
+
+int i;
 
 void setup() {
   motor1.run(RELEASE);
@@ -12,6 +17,8 @@ void setup() {
   motor3.run(RELEASE);
   motor4.run(RELEASE);
   Serial.begin(9600);
+
+  sMotor.attach(13);
 }
 
 void loop() {
@@ -22,7 +29,7 @@ void loop() {
     Serial.print(data);
     
     if(data == 'a'){
-      motor1.run(BACKWARD);
+      motor1.run(FORWARD);
       motor2.run(BACKWARD);
       motor3.run(FORWARD);
       motor4.run(BACKWARD);
@@ -39,7 +46,7 @@ void loop() {
 
     }
     if(data == 'c'){
-      motor1.run(BACKWARD);
+      motor1.run(FORWARD);
       motor4.run(BACKWARD);
       motor1.setSpeed(255);
       motor4.setSpeed(255);
@@ -51,13 +58,13 @@ void loop() {
       motor3.setSpeed(255);
     }
     if(data == 'e'){
-      motor1.run(FORWARD);
+      motor1.run(BACKWARD);
       motor4.run(FORWARD);
       motor1.setSpeed(255);
       motor4.setSpeed(255);
     }
     if(data == 'f'){
-      motor1.run(FORWARD);
+      motor1.run(BACKWARD);
       motor2.run(FORWARD);
       motor3.run(BACKWARD);
       motor4.run(FORWARD);
@@ -71,6 +78,32 @@ void loop() {
       motor2.run(RELEASE);
       motor3.run(RELEASE);
       motor4.run(RELEASE);
+    }
+
+    if(data == 't'){
+      for(i = 0; i < 179; i++){
+        sMotor.write(i);
+        data = Serial.read();
+        if(data == 'z'){
+          break;
+        }
+        delay(500);
+      }
+    }
+
+    if(data == 'u'){
+      for(i = 180; i>0; i--){
+        sMotor.write(i);
+        data = Serial.read();
+        if(data == 'z'){
+          break;
+        }
+        delay(500);
+      }
+    }
+
+    if(data == 'x'){
+      sMotor.write(90);
     }
     
   }
